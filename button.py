83ba225523e4145058dc06a5792b43e317ad09b6
Johan Antony <j6antony@uwaterloo.ca>
2025-12-30 13:54:50 -0500

    fixed button class

--- button.py
+++ button.py
@@ -1,30 +1,31 @@
 import pygame
+
 class Button:
-    def __init__(self, x, y, width, height, text, colour, hover, action):
+    def __init__(self, x, y, width, height, text, colour, hover_colour, action):
         self.color = colour
-        self.hover = hover
+        self.hover_colour = hover_colour
         self.current_colour = colour
+
         self.rect = pygame.Rect(x, y, width, height)
         self.text = text
         self.action = action
-    def draw (self, screen):
+
+        self.font = pygame.font.SysFont(None, 24)
+
+    def draw(self, screen):
         pygame.draw.rect(screen, self.current_colour, self.rect, 3)
-        text_surface = pygame.font.render(self.text, True, (0, 0, 0))
+
+        text_surface = self.font.render(self.text, True, (0, 0, 0))
         text_rect = text_surface.get_rect(center=self.rect.center)
         screen.blit(text_surface, text_rect)
-    def clicked (self, event):
-        if event == pygame.MOUSEBUTTONDOWN:
-            if self.rect.collidepoint:
-                self.action
-            return True
-        return False
-    def hover (self, mouse_pos):
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.action()
+
+    def update_hover(self, mouse_pos):
         if self.rect.collidepoint(mouse_pos):
-            self.current_colour = self.hover
+            self.current_colour = self.hover_colour
         else:
             self.current_colour = self.color
-
-
-
-
-
